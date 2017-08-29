57752e282f8c2b616172db91937064d2482ee413
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2017-08-29 08:53:12 -0500

    Header Content Created

--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import Card from './Card';
+import CardSection from './CardSection';
 
 const AlbumDetail = (props) => {
   return (
-    <View>
-      <Text>{props.album.title}</Text>
-    </View>
+    <Card>
+
+      <CardSection>
+        <View style={{ justifyContent: 'flex-start', flexDirection: 'row' }}>
+          <View>
+            <Image
+              source={{ uri: props.album.thumbnail_image }}
+              style={{ width: 50, height: 50 }}
+            />
+          </View>
+
+          <View style={styles.contentStyle}>
+            <Text>{props.album.title}</Text>
+            <Text>{props.album.artist}</Text>
+          </View>
+        </View>
+      </CardSection>
+
+    </Card>
   );
 };
+
+const styles = {
+  contentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  }
+}
 
 export default AlbumDetail;
