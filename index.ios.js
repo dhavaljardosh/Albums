19f0c0048c4df558c13d4c80043fc73c8e99357b
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2017-08-30 20:02:24 -0500

    App Completed

--- index.ios.js
+++ index.ios.js
@@ -4,7 +4,7 @@ import Header from './src/components/header';
 import AlbumList from './src/components/AlbumList';
 //Create Component
 const App = () => (
-  <View>
+  <View style={{ flex: 1 }}>
     <Header headerText={'Album'} />
     <AlbumList />
   </View>
--- src/components/AlbumDetails.js
+++ src/components/AlbumDetails.js
@@ -1,27 +1,50 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, Linking } from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+  const {
+    thumbnail_image,
+    image,
+    title,
+    artist,
+    url
+  } = album;
+  const {
+    thumbnailContainerStyle,
+    headerTextStyle,
+    headerContentStyle,
+    imageStyle
+  } = styles;
+
   return (
     <Card>
 
       <CardSection>
-        <View style={{ justifyContent: 'flex-start', flexDirection: 'row' }}>
-          <View>
+          <View style={thumbnailContainerStyle}>
             <Image
-              source={{ uri: props.album.thumbnail_image }}
+              source={{ uri: thumbnail_image }}
               style={{ width: 50, height: 50 }}
             />
           </View>
 
-          <View style={styles.contentStyle}>
-            <Text>{props.album.title}</Text>
-            <Text>{props.album.artist}</Text>
+          <View style={headerContentStyle}>
+            <Text style={headerTextStyle}>{title}</Text>
+            <Text>{artist}</Text>
           </View>
-        </View>
+      </CardSection>
+
+      <CardSection>
+        <Image source={{ uri: image }} style={imageStyle} />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => Linking.openURL(url)} >
+          Buy Now
+        </Button>
       </CardSection>
 
     </Card>
@@ -29,9 +52,23 @@ const AlbumDetail = (props) => {
 };
 
 const styles = {
-  contentStyle: {
+  headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    flex: 1,
+    width: null,
+    height: 300
   }
 }
 
